9bebf37c3bca065195104c133d94c58ec697056f
Ekemini Stephen <santacbax@gmail.com>
2022-02-18 15:26:27 +0100

    MVP app commit

--- app/src/main/java/com/example/samplemvp/data/User.kt
+++ app/src/main/java/com/example/samplemvp/data/User.kt
@@ -3,7 +3,7 @@ package com.example.samplemvp.data
 data class User(val name: String, val email: String) {
     private var id: Int = 0
 
-    @JvmName("setId1")
+    @JvmName("setId")
     fun setId(id: Int){
         this.id = id
     }
